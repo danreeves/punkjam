11d53e0e01bf55e33f865671d4cfc91f0cd71d36
Dan Reeves <heydanreeves@gmail.com>
2015-05-19 00:10:36 +0100

    cool stuff, characters, menu

--- src/modules/playerMovement.js
+++ src/modules/playerMovement.js
@@ -1,22 +1,17 @@
-var RUN_SPEED = 3500,
-    JUMP_V = 1000,
-    AIR_DECEL = 0.33,
-    AIR_DRAG = 0,
-    FLOOR_DRAG = 5000;
 
 module.exports = function (player, cursors) {
 
     if (cursors.left.isDown)
     {
         //  Move to the left
-        player.body.acceleration.x = -Math.abs(RUN_SPEED);
+        player.body.acceleration.x = -Math.abs(player.RUN_SPEED);
         player.scale.x = -Math.abs(player.scale.x);
         player.animations.play('run');
     }
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        player.body.acceleration.x = Math.abs(RUN_SPEED);
+        player.body.acceleration.x = Math.abs(player.RUN_SPEED);
         player.scale.x = Math.abs(player.scale.x);
         player.animations.play('run');
     }
@@ -30,16 +25,16 @@ module.exports = function (player, cursors) {
 
     if (!player.body.touching.down) {
         player.animations.play('jump');
-        player.body.acceleration.x = player.body.acceleration.x * AIR_DECEL;
-        player.body.drag.setTo(AIR_DRAG, 0);
+        player.body.acceleration.x = player.body.acceleration.x * player.AIR_DECEL;
+        player.body.drag.setTo(player.AIR_DRAG, 0);
     } else {
-        player.body.drag.setTo(FLOOR_DRAG, 0);
+        player.body.drag.setTo(player.FLOOR_DRAG, 0);
     }
 
     //  Allow the player to jump if they are touching the ground.
     if (cursors.up.isDown && player.body.touching.down)
     {
-        player.body.velocity.y = -Math.abs(JUMP_V);
+        player.body.velocity.y = -Math.abs(player.JUMP_V);
         player.jumps++;
         if (player.firstJump == null) {
             player.firstJump = this.time.now;
